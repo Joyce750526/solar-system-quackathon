1000c12a56e239e96e4bad94d19d0adb02f11c25
Ashelle Cancio <vermillion707@gmail.com>
2023-03-28 13:44:04 -0700

    Added active class to tabs.

--- solar-system-quackathon/src/components/infobox.jsx
+++ solar-system-quackathon/src/components/infobox.jsx
@@ -14,28 +14,53 @@ export default function InfoBox() {
     <div className='container'>
       <div className='tab-container'>
         <ul className='tabs'>
-          <li onClick={() => handleClick("size")}>Size</li>
-          <li onClick={() => handleClick("distance")}>Distance</li>
-          <li onClick={() => handleClick("facts")}>Facts</li>
-          <li onClick={() => handleClick("state")}>Gas/Solid</li>
-          <li onClick={() => handleClick("moons")}>Moons</li>
+          <li
+            onClick={() => handleClick("size")}
+            className={activeTab === "size" ? "active-1" : ""}
+          >
+            Size
+          </li>
+          <li
+            onClick={() => handleClick("distance")}
+            className={activeTab === "distance" ? "active-2" : ""}
+          >
+            Distance
+          </li>
+          <li
+            onClick={() => handleClick("facts")}
+            className={activeTab === "facts" ? "active-2" : ""}
+          >
+            Facts
+          </li>
+          <li
+            onClick={() => handleClick("state")}
+            className={activeTab === "state" ? "active-2" : ""}
+          >
+            Gas/Solid
+          </li>
+          <li
+            onClick={() => handleClick("moons")}
+            className={activeTab === "moons" ? "active-2" : ""}
+          >
+            Moons
+          </li>
         </ul>
-        <div className='tab-content'>
-          <div className={`size ${activeTab === "size" ? "" : "hidden"}`}>
-            Size content
-          </div>
-          <div className={`size ${activeTab === "distance" ? "" : "hidden"}`}>
-            Distance content
-          </div>
-          <div className={`size ${activeTab === "facts" ? "" : "hidden"}`}>
-            Facts content
-          </div>
-          <div className={`size ${activeTab === "state" ? "" : "hidden"}`}>
-            Gas/solid content
-          </div>
-          <div className={`size ${activeTab === "moons" ? "" : "hidden"}`}>
-            Moon(s) content
-          </div>
+      </div>
+      <div className='tab-content'>
+        <div className={`size ${activeTab === "size" ? "" : "hidden"}`}>
+          Size content
+        </div>
+        <div className={`size ${activeTab === "distance" ? "" : "hidden"}`}>
+          Distance content
+        </div>
+        <div className={`size ${activeTab === "facts" ? "" : "hidden"}`}>
+          Facts content
+        </div>
+        <div className={`size ${activeTab === "state" ? "" : "hidden"}`}>
+          Gas/solid content
+        </div>
+        <div className={`size ${activeTab === "moons" ? "" : "hidden"}`}>
+          Moon(s) content
         </div>
       </div>
     </div>
