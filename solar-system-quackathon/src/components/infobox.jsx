43a306bcd70a256e04d38331beb3833634db40d6
Ashelle Cancio <vermillion707@gmail.com>
2023-03-28 14:27:18 -0700

    Added fade in anim to tab content.

--- solar-system-quackathon/src/components/infobox.jsx
+++ solar-system-quackathon/src/components/infobox.jsx
@@ -16,50 +16,68 @@ export default function InfoBox() {
         <ul className='tabs'>
           <li
             onClick={() => handleClick("size")}
-            className={activeTab === "size" ? "active-1" : ""}
+            className={activeTab === "size" ? "bg-1" : ""}
           >
             Size
           </li>
           <li
             onClick={() => handleClick("distance")}
-            className={activeTab === "distance" ? "active-2" : ""}
+            className={activeTab === "distance" ? "bg-2" : ""}
           >
             Distance
           </li>
           <li
             onClick={() => handleClick("facts")}
-            className={activeTab === "facts" ? "active-2" : ""}
+            className={activeTab === "facts" ? "bg-2" : ""}
           >
             Facts
           </li>
           <li
             onClick={() => handleClick("state")}
-            className={activeTab === "state" ? "active-2" : ""}
+            className={activeTab === "state" ? "bg-2" : ""}
           >
             Gas/Solid
           </li>
           <li
             onClick={() => handleClick("moons")}
-            className={activeTab === "moons" ? "active-2" : ""}
+            className={activeTab === "moons" ? "bg-2" : ""}
           >
             Moons
           </li>
         </ul>
       </div>
       <div className='tab-content'>
-        <div className={`size ${activeTab === "size" ? "" : "hidden"}`}>
+        <div
+          className={`size ${activeTab === "size" ? "fade-in-text" : "hidden"}`}
+        >
           Size content
         </div>
-        <div className={`size ${activeTab === "distance" ? "" : "hidden"}`}>
+        <div
+          className={`size ${
+            activeTab === "distance" ? "fade-in-text" : "hidden"
+          }`}
+        >
           Distance content
         </div>
-        <div className={`size ${activeTab === "facts" ? "" : "hidden"}`}>
+        <div
+          className={`size ${
+            activeTab === "facts" ? "fade-in-text" : "hidden"
+          }`}
+        >
           Facts content
         </div>
-        <div className={`size ${activeTab === "state" ? "" : "hidden"}`}>
+        <div
+          className={`size ${
+            activeTab === "state" ? "fade-in-text" : "hidden"
+          }`}
+        >
           Gas/solid content
         </div>
-        <div className={`size ${activeTab === "moons" ? "" : "hidden"}`}>
+        <div
+          className={`size ${
+            activeTab === "moons" ? "fade-in-text" : "hidden"
+          }`}
+        >
           Moon(s) content
         </div>
       </div>
