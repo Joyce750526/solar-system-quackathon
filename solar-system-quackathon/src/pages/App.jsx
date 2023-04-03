900cccfcc825de8fc36f12780b87c9e5fb453e06
Ashelle Cancio <vermillion707@gmail.com>
2023-04-03 14:06:10 -0700

    refactored routing in App.jsx

--- solar-system-quackathon/src/pages/App.jsx
+++ solar-system-quackathon/src/pages/App.jsx
@@ -1,45 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./home";
-import PlaceholderInfo from "./placeholder-info";
+import EarthInfo from "./earth-info";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route
-          path='/mercury/:postId'
-          element={<PlaceholderInfo planet='mercury' />}
-        />
-        <Route
-          path='/venus/:postId'
-          element={<PlaceholderInfo planet='venus' />}
-        />
-        <Route
-          path='/earth/:postId'
-          element={<PlaceholderInfo planet='earth' />}
-        />
-        <Route
-          path='/mars/:postId'
-          element={<PlaceholderInfo planet='mars' />}
-        />
-        <Route
-          path='/jupiter/:postId'
-          element={<PlaceholderInfo planet='jupiter' />}
-        />
-        <Route
-          path='/saturn/:postId'
-          element={<PlaceholderInfo planet='saturn' />}
-        />
-        <Route
-          path='/uranus/:postId'
-          element={<PlaceholderInfo planet='uranus' />}
-        />
-        <Route
-          path='/neptune/:postId'
-          element={<PlaceholderInfo planet='neptune' />}
-        />
+        <Route path='/earth/:postId' element={<EarthInfo />} />
       </Routes>
     </BrowserRouter>
   );
