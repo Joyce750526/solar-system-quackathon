ed8ac10c62df803bb77a2ac095b87d4e346a25c0
Maye Jesuorobo <nayawes@yahoo.com>
2023-04-06 19:18:25 +0200

    brought in cors, created an api route and a server route

--- solar-system-quackathon/server-side/server.js
+++ solar-system-quackathon/server-side/server.js
@@ -1,13 +1,16 @@
 import express from "express";
 const app = express();
 import mongoose from "mongoose";
-// import cors from "cors"
+import cors from "cors"
 import dotenv from "dotenv";
 import homeRoute from "./routes/home.js";
+import reactRouter from "./routes/reactRouter.js"
 const PORT = process.env.PORT || 3000;
 
 //*Import functions/routes
 import connectDB from "./config/database.js";
+
+app.use(cors());
 
 dotenv.config();
 
@@ -23,4 +26,9 @@ app.use(express.json());
 //* ROUTES
 app.use("/", homeRoute);
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.use('/getServer', reactRouter)
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
